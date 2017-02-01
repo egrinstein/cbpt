3f839e40586392e4b163873f96b27fd50555a711
Eric Grinstein <ericgrinstein@gmail.com>
2017-02-01 21:52:33 +0100

    changes viz, prediction model boundaries

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 
 import cv2
 from particle_filter import ParticleFilter
+import numpy as np
 
+def create_legend(img,pt1,pt2):
+    text1 = "Before resampling"
+    cv2.putText(img,text1, pt1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0))
+    text2 = "After resampling"
+    cv2.putText(img,text2, pt2, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255))
+    
 def main():
 
     cap = cv2.VideoCapture(0)
     ret,frame = cap.read()
     print("MAIN:",frame.shape)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    x,y = 240,420
-    pf = ParticleFilter(x,y,frame,n_particles=800,square_size=80)
+    x,y = 240,320
+    pf = ParticleFilter(x,y,frame,n_particles=1500,square_size=90,
+    						dt=0.20)
+    alpha = 0.5
     while(True):        
         ret, frame = cap.read()
+        orig = np.array(frame)
         img = frame
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        x,y,sq_size = pf.next_state(frame)
-        p1 = (int(x-sq_size/2),int(y-sq_size/2))
-        p2 = (int(x+sq_size/2),int(y+sq_size/2))
+        x,y,sq_size,distrib,distrib_control = pf.next_state(frame)
+        p1 = (int(y-sq_size),int(x-sq_size))
+        p2 = (int(y+sq_size),int(x+sq_size))
         
-        cv2.circle(img, (y,x), sq_size, (255,255,0), thickness=5)        
+        # before resampling
+        for (x2,y2,scale2) in distrib_control:
+            x2 = int(x2)
+            y2 = int(y2)
+            cv2.circle(img, (y2,x2), 1, (255,0,0),thickness=10) 
+        # after resampling
+        for (x1,y1,scale1) in distrib:
+        	x1 = int(x1)
+        	y1 = int(y1)
+        	cv2.circle(img, (y1,x1), 1, (0,0,255),thickness=10) 
+        	
+
+        cv2.rectangle(img,p1,p2,(0,0,255),thickness=5)
+
+        cv2.addWeighted(orig, alpha, img, 1 - alpha,0, img)   
+        create_legend(img,(40,40),(40,20))
+
         cv2.imshow('frame',img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break          
--- particle_filter.py
+++ particle_filter.py
@@ -16,31 +16,32 @@ def get_view(image,x,y,sq_size):
     return view
     
 def calc_hist(image):
-        
-    h= cv2.calcHist(image,[0,1],None,[10,10],[36,180,52,256])
-    return h/np.sum(h)
+    
+
+    mask = cv2.inRange(image, np.array((0., 60.,80.)), np.array((180.,255.,255.)))
+    hist = cv2.calcHist(image,[0,1],None,[10,10],[0,180,0,255])
+    cv2.normalize(hist,hist,norm_type=cv2.NORM_MINMAX)
+    return hist
 
 def comp_hist(hist1,hist2):
 
     lbd = 20
     return np.exp(lbd*np.sum(hist1*hist2))
-
+    
 
 class ParticleFilter(object):
-    def __init__(self,x,y,first_frame,n_particles=1000,dt=0.01,
-    				window_size=(480,640),square_size=20):
+    def __init__(self,x,y,first_frame,n_particles=1000,dt=0.04,
+                    window_size=(480,640),square_size=20):
         self.n_particles = n_particles
         self.n_iter = 0
-        self.square_size = square_size
-        vx = vy = vsquare_size = 0
         self.state = np.array([x,y,square_size]) 
         # state =[X[t],Y[t],S[t],X[t-1],Y[t-1],S[t-1]]
-        self.std_state = np.array([20,20,.5])
+        self.std_state = np.array([15,15,1])
 
         self.window_size = window_size
         
-        self.max_square = window_size[0]/3
-        self.min_square = window_size[0]/10
+        self.max_square = window_size[0]*0.5
+        self.min_square = window_size[0]*0.1
 
         self.A = np.array([[1+dt,0,0],
                            [0,1+dt,0],
@@ -52,7 +53,7 @@ class ParticleFilter(object):
                            [0,0,-dt/4]])
 
         self.particles = init_particles(self.state,n_particles)
-        self.last_particles = init_particles(self.state,n_particles)                                
+        self.last_particles = np.array(self.particles)                                
                                         
         self.hist = calc_hist(get_view(first_frame,x,y,square_size))
         
@@ -69,49 +70,55 @@ class ParticleFilter(object):
         
       
         control_prediction = self.transition()
+        control_prediction = self.filter_borders(control_prediction)
+       
         hists = self.candidate_histograms(control_prediction,frame)
-        
+
         weights = self.compare_histograms(hists,self.hist)
         self.last_particles = np.array(self.particles)
         self.particles = self.resample(control_prediction,weights)
         self.state = np.mean(self.particles,axis=0)
 
 
-        self.last_frame = frame
+        self.last_frame = np.array(frame)
         self.n_iter += 1
-        self.square_size = self.state[2]
         self.hist = calc_hist(get_view(frame,self.state[0],self.state[1],self.state[2]))
         
+
         
-        return int(self.state[0]),int(self.state[1]),int(self.state[2])
+        return int(self.state[0]),int(self.state[1]),int(self.state[2]),self.particles,control_prediction
         
         
     def transition(self):
-        n_state = self.state.shape[0]
-        n_particles = self.particles.shape[0]
-        noises = np.zeros(self.particles.shape)
-        for i in range(n_state):
-            noises[:,i] = self.std_state[i]*np.random.randn(1,n_particles)
-        particles = np.dot(self.A,self.particles.T).T
-        particles = particles + np.dot(self.B,self.last_particles.T).T
-        particles = particles + noises
 
-        particles[:,0] = np.clip(particles[:,0],self.square_size,self.window_size[0]-1-self.square_size,particles[:,0])
-        particles[:,1] = np.clip(particles[:,1],self.square_size,self.window_size[1]-1-self.square_size,particles[:,1])
-        particles[:,2] = np.clip(particles[:,2],self.min_square,self.max_square)
+        n_state = self.state.shape[0]
+        n_particles = self.particles.shape[0]   
+        noises = self.std_state*np.random.randn(n_particles,n_state)
+        particles = np.dot(self.particles,self.A) + np.dot(self.last_particles,self.B) + noises
         return particles
 
     def candidate_histograms(self,predictions,image):
-        views = map(lambda x: get_view(image,x[0],x[1],self.square_size),predictions)
-        return map(calc_hist,views)
+        
+        hists = [] 
+
+        for x in predictions:
+            v = get_view(image,x[0],x[1],x[2])
+            hists.append(calc_hist(v))
+        return hists
+        
     def compare_histograms(self,hists,last_hist):
         
-        weights = list(map(lambda x: comp_hist(x,last_hist),hists))
-        weights = weights/np.sum(weights)
-        return weights
-        
+        weights = np.array(list(map(lambda x: comp_hist(x,last_hist),hists)))
+        return weights/np.sum(weights)
+
     def resample(self,predictions,weights):
         indexes = np.arange(weights.shape[0])
         inds = np.random.choice(indexes,self.n_particles,p=weights)
         return predictions[inds]
-             
+    def filter_borders(self,predictions):  
+        ""
+        np.clip(predictions[:,0],self.state[2]+1,self.window_size[0]-(1+self.state[2]),predictions[:,0])        
+        np.clip(predictions[:,1],self.state[2]+1,self.window_size[1]-(1+self.state[2]),predictions[:,1])
+        np.clip(predictions[:,2],self.min_square,self.max_square,predictions[:,2])
+        
+        return predictions
